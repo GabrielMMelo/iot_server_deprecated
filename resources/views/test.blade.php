1a73cb68c54709bafd8da34c488688868097f694
Gabriel Melo <gabrielmelocomp@gmail.com>
2018-05-27 11:40:39 -0300

    Little changes

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,16 +5,12 @@
 @stop
 
 @section('footer')
-<!--    <script src="{ { asset('js/socket.io.js') } }"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"></script>
+    <script src="{{ asset('js/socket.io.js') }}"></script> 
     <script>
-	console.log("oi");
        // var socket = io('http://localhost:3000');
         var socket = io('http://192.168.0.109:3000');
-	console.log("oi2");
         socket.on("test-channel:App\\Events\\EventoTeste", function(message){
-	    console.log("oi3");
-            // increase the power everytime we load test route
+            // increase the power everytime we load fire route
             $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
         });
     </script>
